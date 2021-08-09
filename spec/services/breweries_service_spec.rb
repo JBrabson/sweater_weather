d9e1dd7f6757f4867580d4a581724f6dac33af8a
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-08-09 09:36:10 -0600

    Test for breweries_service passing.

---
+++ spec/services/breweries_service_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe '' do
+  it 'can search by city and return specified number of breweries ' do
+    location = 'denver'
+    response = BreweriesService.get_breweries(location)
+    expect(response).to be_an(Array)
+
+    brewery_hash = response.first
+    expect(brewery_hash).to have_key(:id)
+    expect(brewery_hash).to have_key(:name)
+    expect(brewery_hash).to have_key(:brewery_type)
+
+    expect(brewery_hash[:id]).to be_an(Integer)
+    expect(brewery_hash[:name]).to be_a(String)
+    expect(brewery_hash[:brewery_type]).to be_a(String)
+
+    expect(brewery_hash[:id]).to eq(8039)
+    expect(brewery_hash[:name]).to eq('10 Barrel Brewing Co - Denver')
+    expect(brewery_hash[:brewery_type]).to eq("large")
+  end
+end
